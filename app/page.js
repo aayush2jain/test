ac5bed11c2b7f0cfb14123b939469d5125579f2e
Aayush Jain <aayushjain1290@gmail.com>
2025-04-03 17:20:15 +0530

    first

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import LoginButton from "@/components/LoginButton";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Next.js Google Login with App Router</h1>
+      <LoginButton />
+    </main>
+  );
+}
---
+++ components/LoginButton.js
@@ -0,0 +1,21 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function LoginButton() {
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      {session ? (
+        <div>
+          <p>Welcome, {session.user.name}</p>
+          <img src={session.user.image} alt="Profile" width={50} />
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in with Google</button>
+      )}
+    </div>
+  );
+}
